2619f815e9c6208a80333b3792c9ea11818a40f3
Andrew Brown <andrew@casabrown.com>
2012-03-24 15:43:19 -0400

    Explain use of DataResource in documentation

--- aima-core/src/main/java/aima/core/learning/data/DataResource.java
+++ aima-core/src/main/java/aima/core/learning/data/DataResource.java
@@ -1,5 +1,9 @@
 package aima.core.learning.data;
 
+/**
+ * Provides access to the learning/data/ directory for loading *.csv files
+ * using class.getResourceAsStream()
+ * @author Andrew Brown
+ */
 public class DataResource {
-
 }
