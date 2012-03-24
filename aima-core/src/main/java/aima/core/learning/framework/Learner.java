caefbb7bf96f136085a778a7933b3cf1785cdea9
Andrew Brown <andrew@casabrown.com>
2012-03-24 17:05:56 -0400

    Document Learner

--- aima-core/src/main/java/aima/core/learning/framework/Learner.java
+++ aima-core/src/main/java/aima/core/learning/framework/Learner.java
@@ -1,29 +1,33 @@
 package aima.core.learning.framework;
 
 /**
+ * Describes a model for learning based on a set of examples; see page 693-694
+ * in AIMAv3 for further explanation. This model does not specify the types 
+ * of feedback (unsupervised, reinforcement, supervised) used to train the 
+ * learner.
+ * @todo should implement aima.core.agent.Agent
  * @author Ravi Mohan
- * 
+ * @author Andrew Brown
  */
 public interface Learner {
-	void train(DataSet ds);
 
-	/**
-	 * Returns the outcome predicted for the specified example
-	 * 
-	 * @param e
-	 *            an example
-	 * 
-	 * @return the outcome predicted for the specified example
-	 */
-	String predict(Example e);
+    /**
+     * Trains the learner given a set of examples
+     * @param training_set the training data set 
+     */
+    void train(DataSet training_set);
 
-	/**
-	 * Returns the accuracy of the hypothesis on the specified set of examples
-	 * 
-	 * @param ds
-	 *            the test data set.
-	 * 
-	 * @return the accuracy of the hypothesis on the specified set of examples
-	 */
-	int[] test(DataSet ds);
+    /**
+     * Returns the outcome predicted for the specified example
+     * @param example an example
+     * @return the outcome predicted for the specified example
+     */
+    String predict(Example example);
+
+    /**
+     * Returns the accuracy of the hypothesis on the specified set of examples
+     * @param test_set the test data set.
+     * @return the accuracy of the hypothesis on the specified set of examples
+     */
+    int[] test(DataSet test_set);
 }
