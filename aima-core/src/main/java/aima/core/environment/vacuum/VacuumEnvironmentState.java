f1b4e40c9bb5251028a1c9fd6207b8c70230c592
Andrew Brown <andrew@casabrown.com>
2012-03-23 23:23:32 -0400

    Override equals(); now Plan.contains() will recognize cyclic states
    Also, improves formatting and documentation

--- aima-core/src/main/java/aima/core/environment/vacuum/VacuumEnvironmentState.java
+++ aima-core/src/main/java/aima/core/environment/vacuum/VacuumEnvironmentState.java
@@ -7,44 +7,102 @@ import aima.core.agent.Agent;
 import aima.core.agent.EnvironmentState;
 
 /**
+ * Represents a state in the Vacuum World
  * @author Ciaran O'Reilly
- * 
  */
 public class VacuumEnvironmentState implements EnvironmentState {
 
-	private Map<String, VacuumEnvironment.LocationState> state;
-	private Map<Agent, String> agentLocations;
+    private Map<String, VacuumEnvironment.LocationState> state;
+    private Map<Agent, String> agentLocations;
 
-	public VacuumEnvironmentState() {
-		state = new LinkedHashMap<String, VacuumEnvironment.LocationState>();
-		agentLocations = new LinkedHashMap<Agent, String>();
-	}
+    /**
+     * Constructor
+     */
+    public VacuumEnvironmentState() {
+        state = new LinkedHashMap<String, VacuumEnvironment.LocationState>();
+        agentLocations = new LinkedHashMap<Agent, String>();
+    }
 
-	public VacuumEnvironmentState(VacuumEnvironment.LocationState locAState,
-			VacuumEnvironment.LocationState locBState) {
-		this();
-		state.put(VacuumEnvironment.LOCATION_A, locAState);
-		state.put(VacuumEnvironment.LOCATION_B, locBState);
-	}
+    /**
+     * Constructor
+     * @param locAState
+     * @param locBState 
+     */
+    public VacuumEnvironmentState(VacuumEnvironment.LocationState locAState,
+            VacuumEnvironment.LocationState locBState) {
+        this();
+        state.put(VacuumEnvironment.LOCATION_A, locAState);
+        state.put(VacuumEnvironment.LOCATION_B, locBState);
+    }
 
-	public String getAgentLocation(Agent a) {
-		return agentLocations.get(a);
-	}
+    /**
+     * Returns the agent location
+     * @param a
+     * @return 
+     */
+    public String getAgentLocation(Agent a) {
+        return agentLocations.get(a);
+    }
 
-	public void setAgentLocation(Agent a, String location) {
-		agentLocations.put(a, location);
-	}
+    /**
+     * Sets the agent location
+     * @param a
+     * @param location 
+     */
+    public void setAgentLocation(Agent a, String location) {
+        agentLocations.put(a, location);
+    }
 
-	public VacuumEnvironment.LocationState getLocationState(String location) {
-		return state.get(location);
-	}
+    /**
+     * Returns the location state
+     * @param location
+     * @return 
+     */
+    public VacuumEnvironment.LocationState getLocationState(String location) {
+        return state.get(location);
+    }
 
-	public void setLocationState(String location,
-			VacuumEnvironment.LocationState s) {
-		state.put(location, s);
-	}
+    /**
+     * Sets the location state
+     * @param location
+     * @param s 
+     */
+    public void setLocationState(String location,
+            VacuumEnvironment.LocationState s) {
+        state.put(location, s);
+    }
 
-	public String toString() {
-		return state.toString();
-	}
+    /**
+     * Tests equality against another VacuumEnvironmentState
+     * @param s
+     * @return 
+     */
+    public boolean equals(VacuumEnvironmentState s) {
+        if (!this.state.equals(s.state)) {
+            return false;
+        } else if (!this.agentLocations.equals(s.agentLocations)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Tests equality against another VacuumEnvironmentState;
+     * necessary for AND-OR search to work
+     * @param o
+     * @return 
+     */
+    @Override
+    public boolean equals(Object o) {
+        return this.equals((VacuumEnvironmentState) o);
+    }
+
+    /**
+     * Returns a string representation of the environment
+     * @return 
+     */
+    public String toString() {
+        return this.state.toString();
+    }
 }
