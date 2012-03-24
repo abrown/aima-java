4c63586ada2d60643a5ad719ebbb1587e5794f69
Andrew Brown <andrew@casabrown.com>
2012-03-23 22:14:22 -0400

    Improve formatting and documentation

--- aima-core/src/main/java/aima/core/environment/vacuum/VacuumEnvironment.java
+++ aima-core/src/main/java/aima/core/environment/vacuum/VacuumEnvironment.java
@@ -24,109 +24,131 @@ import aima.core.agent.impl.DynamicAction;
  * @author Mike Stampone
  */
 public class VacuumEnvironment extends AbstractEnvironment {
-	// Allowable Actions within the Vacuum Environment
-	public static final Action ACTION_MOVE_LEFT = new DynamicAction("Left");
-	public static final Action ACTION_MOVE_RIGHT = new DynamicAction("Right");
-	public static final Action ACTION_SUCK = new DynamicAction("Suck");
-	public static final String LOCATION_A = "A";
-	public static final String LOCATION_B = "B";
 
-	public enum LocationState {
-		Clean, Dirty
-	};
+    // Allowable actions within the vacuum environment
+    public static final Action ACTION_MOVE_LEFT = new DynamicAction("Left");
+    public static final Action ACTION_MOVE_RIGHT = new DynamicAction("Right");
+    public static final Action ACTION_SUCK = new DynamicAction("Suck");
+    public static final String LOCATION_A = "A";
+    public static final String LOCATION_B = "B";
 
-	//
-	protected VacuumEnvironmentState envState = null;
-	protected boolean isDone = false;
+    public enum LocationState {
 
-	/**
-	 * Constructs a vacuum environment with two locations, in which dirt is
-	 * placed at random.
-	 */
-	public VacuumEnvironment() {
-		Random r = new Random();
-		envState = new VacuumEnvironmentState(
-				0 == r.nextInt(2) ? LocationState.Clean : LocationState.Dirty,
-				0 == r.nextInt(2) ? LocationState.Clean : LocationState.Dirty);
-	}
+        Clean, Dirty
+    };
+    protected VacuumEnvironmentState envState = null;
+    protected boolean isDone = false;
 
-	/**
-	 * Constructs a vacuum environment with two locations, in which dirt is
-	 * placed as specified.
-	 * 
-	 * @param locAState
-	 *            the initial state of location A, which is either
-	 *            <em>Clean</em> or <em>Dirty</em>.
-	 * @param locBState
-	 *            the initial state of location B, which is either
-	 *            <em>Clean</em> or <em>Dirty</em>.
-	 */
-	public VacuumEnvironment(LocationState locAState, LocationState locBState) {
-		envState = new VacuumEnvironmentState(locAState, locBState);
-	}
+    /**
+     * Constructs a vacuum environment with two locations, in which dirt is
+     * placed at random.
+     */
+    public VacuumEnvironment() {
+        Random r = new Random();
+        envState = new VacuumEnvironmentState(
+                0 == r.nextInt(2) ? LocationState.Clean : LocationState.Dirty,
+                0 == r.nextInt(2) ? LocationState.Clean : LocationState.Dirty);
+    }
 
-	@Override
-	public EnvironmentState getCurrentState() {
-		return envState;
-	}
+    /**
+     * Constructs a vacuum environment with two locations, in which dirt is
+     * placed as specified.
+     * 
+     * @param locAState
+     *            the initial state of location A, which is either
+     *            <em>Clean</em> or <em>Dirty</em>.
+     * @param locBState
+     *            the initial state of location B, which is either
+     *            <em>Clean</em> or <em>Dirty</em>.
+     */
+    public VacuumEnvironment(LocationState locAState, LocationState locBState) {
+        envState = new VacuumEnvironmentState(locAState, locBState);
+    }
 
-	@Override
-	public EnvironmentState executeAction(Agent a, Action agentAction) {
+    @Override
+    public EnvironmentState getCurrentState() {
+        return envState;
+    }
 
-		if (ACTION_MOVE_RIGHT == agentAction) {
-			envState.setAgentLocation(a, LOCATION_B);
-			updatePerformanceMeasure(a, -1);
-		} else if (ACTION_MOVE_LEFT == agentAction) {
-			envState.setAgentLocation(a, LOCATION_A);
-			updatePerformanceMeasure(a, -1);
-		} else if (ACTION_SUCK == agentAction) {
-			if (LocationState.Dirty == envState.getLocationState(envState
-					.getAgentLocation(a))) {
-				envState.setLocationState(envState.getAgentLocation(a),
-						LocationState.Clean);
-				updatePerformanceMeasure(a, 10);
-			}
-		} else if (agentAction.isNoOp()) {
-			// In the Vacuum Environment we consider things done if
-			// the agent generates a NoOp.
-			isDone = true;
-		}
+    @Override
+    public EnvironmentState executeAction(Agent a, Action agentAction) {
+        if (ACTION_MOVE_RIGHT == agentAction) {
+            envState.setAgentLocation(a, LOCATION_B);
+            updatePerformanceMeasure(a, -1);
+        } else if (ACTION_MOVE_LEFT == agentAction) {
+            envState.setAgentLocation(a, LOCATION_A);
+            updatePerformanceMeasure(a, -1);
+        } else if (ACTION_SUCK == agentAction) {
+            if (LocationState.Dirty == envState.getLocationState(envState.getAgentLocation(a))) {
+                envState.setLocationState(envState.getAgentLocation(a),
+                        LocationState.Clean);
+                updatePerformanceMeasure(a, 10);
+            }
+        } else if (agentAction.isNoOp()) {
+            // In the Vacuum Environment we consider things done if
+            // the agent generates a NoOp.
+            isDone = true;
+        }
 
-		return envState;
-	}
+        return envState;
+    }
 
-	@Override
-	public Percept getPerceptSeenBy(Agent anAgent) {
-		String agentLocation = envState.getAgentLocation(anAgent);
-		return new VacuumEnvPercept(agentLocation,
-				envState.getLocationState(agentLocation));
-	}
+    @Override
+    public Percept getPerceptSeenBy(Agent anAgent) {
+        String agentLocation = envState.getAgentLocation(anAgent);
+        return new VacuumEnvPercept(agentLocation,
+                envState.getLocationState(agentLocation));
+    }
 
-	@Override
-	public boolean isDone() {
-		return super.isDone() || isDone;
-	}
+    @Override
+    public boolean isDone() {
+        return super.isDone() || isDone;
+    }
 
-	@Override
-	public void addAgent(Agent a) {
-		int idx = new Random().nextInt(2);
-		envState.setAgentLocation(a, idx == 0 ? LOCATION_A : LOCATION_B);
-		super.addAgent(a);
-	}
+    @Override
+    public void addAgent(Agent a) {
+        int idx = new Random().nextInt(2);
+        envState.setAgentLocation(a, idx == 0 ? LOCATION_A : LOCATION_B);
+        super.addAgent(a);
+    }
 
-	public void addAgent(Agent a, String location) {
-		// Ensure the agent state information is tracked before
-		// adding to super, as super will notify the registered
-		// EnvironmentViews that is was added.
-		envState.setAgentLocation(a, location);
-		super.addAgent(a);
-	}
+    /**
+     * Adds agent to the environment
+     * @param a
+     * @param location 
+     */
+    public void addAgent(Agent a, String location) {
+        // Ensure the agent state information is tracked before
+        // adding to super, as super will notify the registered
+        // EnvironmentViews that is was added.
+        envState.setAgentLocation(a, location);
+        super.addAgent(a);
+    }
 
-	public LocationState getLocationState(String location) {
-		return envState.getLocationState(location);
-	}
+    /**
+     * Returns the location state as an enum
+     * @param location
+     * @return 
+     */
+    public LocationState getLocationState(String location) {
+        return envState.getLocationState(location);
+    }
 
-	public String getAgentLocation(Agent a) {
-		return envState.getAgentLocation(a);
-	}
+    /**
+     * Returns the agent location
+     * @param a
+     * @return 
+     */
+    public String getAgentLocation(Agent a) {
+        return envState.getAgentLocation(a);
+    }
+
+    
+    /**
+     * Returns a string representation of the environment
+     * @return 
+     */
+    public String toString() {
+        return this.envState.toString();
+    }
 }
