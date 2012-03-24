bc46c7d6d3266ec5ef6c712d4510fc8bc50ca560
Andrew Brown <andrew@casabrown.com>
2012-03-24 17:36:47 -0400

    Documents DataSet and adds proposed changes as @todo annotations

--- aima-core/src/main/java/aima/core/learning/framework/DataSet.java
+++ aima-core/src/main/java/aima/core/learning/framework/DataSet.java
@@ -4,166 +4,255 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
-
 import aima.core.util.Util;
 
 /**
+ * Provides methods for representing a set of learning examples
  * @author Ravi Mohan
- * 
+ * @author Andrew Brown
  */
 public class DataSet {
-	protected DataSet() {
 
-	}
+    public List<Example> examples;
+    
+    /**
+     * @todo explain this
+     */
+    public DataSetSpecification specification;
+    
+    /**
+     * Constructor
+     */
+    protected DataSet() {
+        examples = new LinkedList<Example>();
+    }
 
-	public List<Example> examples;
+    /**
+     * Constructor
+     * @param spec 
+     */
+    public DataSet(DataSetSpecification spec) {
+        examples = new LinkedList<Example>();
+        this.specification = spec;
+    }
 
-	public DataSetSpecification specification;
+    /**
+     * Adds an example to the set
+     * @param e 
+     */
+    public void add(Example e) {
+        examples.add(e);
+    }
 
-	public DataSet(DataSetSpecification spec) {
-		examples = new LinkedList<Example>();
-		this.specification = spec;
-	}
+    /**
+     * Returns the size of the set
+     * @return 
+     */
+    public int size() {
+        return examples.size();
+    }
 
-	public void add(Example e) {
-		examples.add(e);
-	}
+    /**
+     * Returns the example given an index
+     * @param number
+     * @return 
+     */
+    public Example getExample(int index) {
+        return examples.get(index);
+    }
 
-	public int size() {
-		return examples.size();
-	}
+    /**
+     * Removes the example given from the data set
+     * @param e
+     * @return 
+     */
+    public DataSet removeExample(Example e) {
+        DataSet ds = new DataSet(specification);
+        for (Example eg : examples) {
+            if (!(e.equals(eg))) {
+                ds.add(eg);
+            }
+        }
+        return ds;
+    }
 
-	public Example getExample(int number) {
-		return examples.get(number);
-	}
+    /**
+     * Returns the distribution
+     * @todo explain better...
+     * @return 
+     */
+    public double getInformationFor() {
+        String attributeName = specification.getTarget();
+        HashMap<String, Integer> counts = new HashMap<String, Integer>();
+        for (Example e : examples) {
+            String val = e.getAttributeValueAsString(attributeName);
+            if (counts.containsKey(val)) {
+                counts.put(val, counts.get(val) + 1);
+            } else {
+                counts.put(val, 1);
+            }
+        }
+        double[] data = new double[counts.keySet().size()];
+        Iterator<Integer> iter = counts.values().iterator();
+        for (int i = 0; i < data.length; i++) {
+            data[i] = iter.next();
+        }
+        data = Util.normalize(data);
+        return Util.information(data);
+    }
 
-	public DataSet removeExample(Example e) {
-		DataSet ds = new DataSet(specification);
-		for (Example eg : examples) {
-			if (!(e.equals(eg))) {
-				ds.add(eg);
-			}
-		}
-		return ds;
-	}
+    /**
+     * Splits the set by attribute
+     * @param attributeName
+     * @return 
+     */
+    public HashMap<String, DataSet> splitByAttribute(String attributeName) {
+        HashMap<String, DataSet> results = new HashMap<String, DataSet>();
+        for (Example e : examples) {
+            String val = e.getAttributeValueAsString(attributeName);
+            if (results.containsKey(val)) {
+                results.get(val).add(e);
+            } else {
+                DataSet ds = new DataSet(specification);
+                ds.add(e);
+                results.put(val, ds);
+            }
+        }
+        return results;
+    }
 
-	public double getInformationFor() {
-		String attributeName = specification.getTarget();
-		HashMap<String, Integer> counts = new HashMap<String, Integer>();
-		for (Example e : examples) {
+    /**
+     * @todo explain better
+     * @param parameterName
+     * @return 
+     */
+    public double calculateGainFor(String parameterName) {
+        HashMap<String, DataSet> hash = splitByAttribute(parameterName);
+        double totalSize = examples.size();
+        double remainder = 0.0;
+        for (String parameterValue : hash.keySet()) {
+            double reducedDataSetSize = hash.get(parameterValue).examples.size();
+            remainder += (reducedDataSetSize / totalSize)
+                    * hash.get(parameterValue).getInformationFor();
+        }
+        return getInformationFor() - remainder;
+    }
 
-			String val = e.getAttributeValueAsString(attributeName);
-			if (counts.containsKey(val)) {
-				counts.put(val, counts.get(val) + 1);
-			} else {
-				counts.put(val, 1);
-			}
-		}
+    /**
+     * Overrides equals() to compare with uncast Object
+     * @param o
+     * @return 
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if ((o == null) || (this.getClass() != o.getClass())) {
+            return false;
+        }
+        DataSet other = (DataSet) o;
+        return examples.equals(other.examples);
+    }
 
-		double[] data = new double[counts.keySet().size()];
-		Iterator<Integer> iter = counts.values().iterator();
-		for (int i = 0; i < data.length; i++) {
-			data[i] = iter.next();
-		}
-		data = Util.normalize(data);
+    /**
+     * For use in equals()
+     * @return 
+     */
+    @Override
+    public int hashCode() {
+        return 0;
+    }
 
-		return Util.information(data);
-	}
+    /**
+     * Iterator
+     * @return 
+     */
+    public Iterator<Example> iterator() {
+        return examples.iterator();
+    }
 
-	public HashMap<String, DataSet> splitByAttribute(String attributeName) {
-		HashMap<String, DataSet> results = new HashMap<String, DataSet>();
-		for (Example e : examples) {
-			String val = e.getAttributeValueAsString(attributeName);
-			if (results.containsKey(val)) {
-				results.get(val).add(e);
-			} else {
-				DataSet ds = new DataSet(specification);
-				ds.add(e);
-				results.put(val, ds);
-			}
-		}
-		return results;
-	}
+    /**
+     * Copies this set to a new object
+     * @return 
+     */
+    public DataSet copy() {
+        DataSet ds = new DataSet(specification);
+        for (Example e : examples) {
+            ds.add(e);
+        }
+        return ds;
+    }
 
-	public double calculateGainFor(String parameterName) {
-		HashMap<String, DataSet> hash = splitByAttribute(parameterName);
-		double totalSize = examples.size();
-		double remainder = 0.0;
-		for (String parameterValue : hash.keySet()) {
-			double reducedDataSetSize = hash.get(parameterValue).examples
-					.size();
-			remainder += (reducedDataSetSize / totalSize)
-					* hash.get(parameterValue).getInformationFor();
-		}
-		return getInformationFor() - remainder;
-	}
+    /**
+     * Returns the attribute names
+     * @return 
+     */
+    public List<String> getAttributeNames() {
+        return specification.getAttributeNames();
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o) {
-			return true;
-		}
-		if ((o == null) || (this.getClass() != o.getClass())) {
-			return false;
-		}
-		DataSet other = (DataSet) o;
-		return examples.equals(other.examples);
-	}
+    /**
+     * Returns the target attribute name
+     * @return 
+     */
+    public String getTargetAttributeName() {
+        return specification.getTarget();
+    }
 
-	@Override
-	public int hashCode() {
-		return 0;
-	}
+    /**
+     * Returns a new empty data set
+     * @todo move to factory
+     * @return 
+     */
+    public DataSet emptyDataSet() {
+        return new DataSet(specification);
+    }
 
-	public Iterator<Example> iterator() {
-		return examples.iterator();
-	}
+    /**
+     * Sets the specification for this object
+     * @todo remove this
+     * @param specification the specification to set. USE SPARINGLY for testing 
+     * etc .. makes no semantic sense
+     */
+    public void setSpecification(DataSetSpecification specification) {
+        this.specification = specification;
+    }
 
-	public DataSet copy() {
-		DataSet ds = new DataSet(specification);
-		for (Example e : examples) {
-			ds.add(e);
-		}
-		return ds;
-	}
+    /**
+     * Wrapper for specification.getPossibleAttributeValues()
+     * @todo remove this
+     * @param attributeName
+     * @return 
+     */
+    public List<String> getPossibleAttributeValues(String attributeName) {
+        return specification.getPossibleAttributeValues(attributeName);
+    }
 
-	public List<String> getAttributeNames() {
-		return specification.getAttributeNames();
-	}
+    /**
+     * Filters the set, returning only the results that match the given 
+     * attribute name and value
+     * @todo rename as filter()
+     * @param attributeName
+     * @param attributeValue
+     * @return 
+     */
+    public DataSet matchingDataSet(String attributeName, String attributeValue) {
+        DataSet ds = new DataSet(specification);
+        for (Example e : examples) {
+            if (e.getAttributeValueAsString(attributeName).equals(
+                    attributeValue)) {
+                ds.add(e);
+            }
+        }
+        return ds;
+    }
 
-	public String getTargetAttributeName() {
-		return specification.getTarget();
-	}
-
-	public DataSet emptyDataSet() {
-		return new DataSet(specification);
-	}
-
-	/**
-	 * @param specification
-	 *            The specification to set. USE SPARINGLY for testing etc ..
-	 *            makes no semantic sense
-	 */
-	public void setSpecification(DataSetSpecification specification) {
-		this.specification = specification;
-	}
-
-	public List<String> getPossibleAttributeValues(String attributeName) {
-		return specification.getPossibleAttributeValues(attributeName);
-	}
-
-	public DataSet matchingDataSet(String attributeName, String attributeValue) {
-		DataSet ds = new DataSet(specification);
-		for (Example e : examples) {
-			if (e.getAttributeValueAsString(attributeName).equals(
-					attributeValue)) {
-				ds.add(e);
-			}
-		}
-		return ds;
-	}
-
-	public List<String> getNonTargetAttributes() {
-		return Util.removeFrom(getAttributeNames(), getTargetAttributeName());
-	}
+    /**
+     * Returns all attributes that are not the target
+     * @return 
+     */
+    public List<String> getNonTargetAttributes() {
+        return Util.removeFrom(getAttributeNames(), getTargetAttributeName());
+    }
 }
