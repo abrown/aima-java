6bb34e93ef1d494a91210366b2872e71a95d347d
Andrew Brown <andrew@casabrown.com>
2012-04-08 21:37:20 -0400

    Improve: make iterable and add custom equals()

--- aima-core/src/main/java/aima/core/learning/inductive/DecisionListTest.java
+++ aima-core/src/main/java/aima/core/learning/inductive/DecisionListTest.java
@@ -3,6 +3,7 @@ package aima.core.learning.inductive;
 import aima.core.learning.framework.Attribute;
 import aima.core.learning.framework.DataSet;
 import aima.core.learning.framework.Example;
+import java.util.Iterator;
 import java.util.LinkedList;
 
 /**
@@ -11,7 +12,7 @@ import java.util.LinkedList;
  * @author Ravi Mohan
  * @author Andrew Brown
  */
-public class DecisionListTest {
+public class DecisionListTest implements Iterable<Attribute>{
 
     /**
      * The test is a conjunction; every attribute in the test is a conjunct 
@@ -33,6 +34,15 @@ public class DecisionListTest {
      */
     public void add(Attribute attribute) {
         this.conjuncts.add(attribute);
+    }
+    
+    /**
+     * Test whether a conjunct exists
+     * @param attribute
+     * @return 
+     */
+    public boolean contains(Attribute attribute){
+        return this.conjuncts.contains(attribute);
     }
 
     /**
@@ -104,5 +114,26 @@ public class DecisionListTest {
         s.append("]");
         // return
         return s.toString();
+    }
+    
+    /**
+     * Make the test iterable over its attributes/conjuncts
+     * @return 
+     */
+    public Iterator<Attribute> iterator(){
+        return this.conjuncts.iterator();
+    }
+    
+    /**
+     * Determines whether two tests are equal; that is if they test the same
+     * attributes.
+     * @param other
+     * @return 
+     */
+    public boolean equals(DecisionListTest other){
+        for(Attribute a: this){
+            if( !other.contains(a)) return false;
+        }
+        return true;
     }
 }
