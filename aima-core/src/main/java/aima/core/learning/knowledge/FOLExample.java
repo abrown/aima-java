ae6c734702988510f1bfbe34b94ade3d56d0ec1d
andrew <andrew@casabrown.com>
2012-04-20 11:17:28 -0400

    Format properly

--- aima-core/src/main/java/aima/core/learning/knowledge/FOLExample.java
+++ aima-core/src/main/java/aima/core/learning/knowledge/FOLExample.java
@@ -1,57 +1,57 @@
 package aima.core.learning.knowledge;
+
 import aima.core.learning.framework.Attribute;
-import java.util.ArrayList;
-import java.util.List;
 import aima.core.learning.framework.Example;
 import aima.core.logic.fol.Connectors;
-import aima.core.logic.fol.parsing.ast.ConnectedSentence;
-import aima.core.logic.fol.parsing.ast.Constant;
-import aima.core.logic.fol.parsing.ast.NotSentence;
-import aima.core.logic.fol.parsing.ast.Predicate;
-import aima.core.logic.fol.parsing.ast.Sentence;
-import aima.core.logic.fol.parsing.ast.Term;
+import aima.core.logic.fol.parsing.ast.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Extends Example to work with CURRENT-BEST-LEARNING (page 771, AIMAv3). This
  * class converts the attributes in an Example to Predicates in a first order
  * logic sentence.
+ *
  * @author Ciaran O'Reilly
  * @author Andrew Brown
  */
 public class FOLExample extends Example {
 
     public String classificationPredicate = "UnknownClassifier";
-    
+
     /**
-     * Returns the constant representing this example; uses hashCode() as it
-     * is the most convenient pattern in Java for uniquely representing this 
+     * Returns the constant representing this example; uses hashCode() as it is
+     * the most convenient pattern in Java for uniquely representing this
      * object.
-     * @return 
+     *
+     * @return
      */
-    public Constant toConstant(){
-        return new Constant("X_"+this.hashCode());
+    public Constant toConstant() {
+        return new Constant("X_" + this.hashCode());
     }
-    
+
     /**
-     * Returns the classification sentence for this example; e.g. 
+     * Returns the classification sentence for this example; e.g.
      * "WillWait(X_83478)" or "NOT WillWait(X_83478)".
+     *
      * @param classificationPredicate
-     * @return 
+     * @return
      */
     public Sentence toClassification() {
         List<Term> terms = new ArrayList<Term>();
         terms.add(this.toConstant());
         Sentence classification = new Predicate(this.classificationPredicate, terms);
-        if (this.getOutput() == null){
+        if (this.getOutput() == null) {
             classification = new NotSentence(classification);
         }
         return classification;
     }
 
     /**
-     * Returns the Example as a first order logic sentence; e.g. "Patrons(X_1234, Full)
-     * AND NOT Hungry(X_1234)".
-     * @return 
+     * Returns the Example as a first order logic sentence; e.g.
+     * "Patrons(X_1234, Full) AND NOT Hungry(X_1234)".
+     *
+     * @return
      */
     public Sentence toDescription() {
         // collect predicates for this example
@@ -62,20 +62,18 @@ public class FOLExample extends Example {
             List<Term> terms = new ArrayList<Term>();
             terms.add(this.toConstant());
             // single value predicate
-            if( a.getValue() instanceof Boolean ){
+            if (a.getValue() instanceof Boolean) {
                 boolean value = (Boolean) a.getValue();
-                if( value ){
+                if (value) {
                     // do nothing; e.g. Predicate(X_1385)
                     predicate = new Predicate(a.getName(), terms);
-                }
-                else{
+                } else {
                     // create not-sentence
                     predicate = new Predicate(a.getName(), terms);
                     predicate = new NotSentence(predicate);
                 }
-            }
-            // multi-value predicate
-            else{
+            } // multi-value predicate
+            else {
                 Constant value = new Constant(a.getValue().toString());
                 terms.add(value);
                 predicate = new Predicate(a.getName(), terms);
@@ -85,13 +83,12 @@ public class FOLExample extends Example {
         }
         // compile as ConnectedSentence
         Sentence output = null;
-        if(predicates.size() > 1){
+        if (predicates.size() > 1) {
             output = new ConnectedSentence(Connectors.AND, predicates.get(0), predicates.get(1));
             for (int i = 2; i < predicates.size(); i++) {
                 output = new ConnectedSentence(Connectors.AND, output, predicates.get(i));
             }
-        }
-        else{
+        } else {
             output = predicates.get(0);
         }
         // return
--- aima-core/src/main/java/aima/core/learning/knowledge/FOLHypothesis.java
+++ aima-core/src/main/java/aima/core/learning/knowledge/FOLHypothesis.java
@@ -1,4 +1,5 @@
 package aima.core.learning.knowledge;
+
 import aima.core.logic.fol.Connectors;
 import aima.core.logic.fol.parsing.ast.ConnectedSentence;
 import aima.core.logic.fol.parsing.ast.Predicate;
@@ -12,41 +13,45 @@ import java.util.List;
 /**
  * Represents a Hypothesis; per page 769, AIMAv3, should be in the form:
  * <pre>
- * FORALL v (Classification(v) &lt;=&gt; 
+ * FORALL v (Classification(v) &lt;=&gt;
  *  (Description1(v) AND Description2(v, Constant1))
  *  OR (Description1(v) AND Description3(v))
  *  OR ...
  * )
  * </pre>
+ *
  * @author Ciaran O'Reilly
  * @author Andrew Brown
  */
-public class FOLHypothesis extends QuantifiedSentence{
+public class FOLHypothesis extends QuantifiedSentence {
 
     /**
      * Constructor
-     * @param classificationPredicate 
+     *
+     * @param classificationPredicate
      */
     public FOLHypothesis(String classificationPredicate) {
         super("FORALL", buildVariables(), buildSentence(classificationPredicate));
     }
-    
+
     /**
      * Return "r"; see page 769, AIMAv3
-     * @return 
+     *
+     * @return
      */
-    private static List<Variable> buildVariables(){
+    private static List<Variable> buildVariables() {
         ArrayList<Variable> variables = new ArrayList<Variable>();
         variables.add(new Variable("r"));
         return variables;
     }
-    
+
     /**
      * Return starting sentence; e.g. "FORALL r WillWait(r) <=> true"
+     *
      * @param predicate
-     * @return 
+     * @return
      */
-    private static Sentence buildSentence(String predicate){
+    private static Sentence buildSentence(String predicate) {
         // build predicate
         ArrayList<Term> terms = new ArrayList<Term>();
         terms.add(new Variable("r"));
