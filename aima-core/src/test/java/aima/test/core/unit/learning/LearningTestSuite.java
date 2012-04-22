f7b60f9a543947050b7d90be6b929edb601b0602
andrew <andrew@casabrown.com>
2012-04-22 13:33:55 -0400

    Update with current tests in suite

--- aima-core/src/test/java/aima/test/core/unit/learning/LearningTestSuite.java
+++ aima-core/src/test/java/aima/test/core/unit/learning/LearningTestSuite.java
@@ -3,25 +3,38 @@ package aima.test.core.unit.learning;
 import aima.test.core.unit.learning.framework.AttributeTest;
 import aima.test.core.unit.learning.framework.DataSetTest;
 import aima.test.core.unit.learning.framework.ExampleTest;
-import aima.test.core.unit.learning.inductive.DLTestTest;
 import aima.test.core.unit.learning.inductive.DecisionListTest;
-import aima.test.core.unit.learning.learners.DecisionTreeTest;
-import aima.test.core.unit.learning.learners.EnsembleLearningTest;
-import aima.test.core.unit.learning.learners.LearnerTests;
+import aima.test.core.unit.learning.inductive.DecisionTreeTest;
+import aima.test.core.unit.learning.learners.*;
 import aima.test.core.unit.learning.neural.NeuralNetworkTest;
 import aima.test.core.unit.learning.neural.PerceptronTest;
 import aima.test.core.unit.learning.reinforcement.agent.PassiveADPAgentTest;
 import aima.test.core.unit.learning.reinforcement.agent.PassiveTDAgentTest;
 import aima.test.core.unit.learning.reinforcement.agent.QLearningAgentTest;
+import aima.test.core.unit.learning.reinforcement.agent.ReinforcementLearningAgentTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({DataSetTest.class, ExampleTest.class, AttributeTest.class,
-    DecisionListTest.class, DLTestTest.class, DecisionTreeTest.class,
-    EnsembleLearningTest.class, LearnerTests.class,
-    NeuralNetworkTest.class, PerceptronTest.class,
-    PassiveADPAgentTest.class, PassiveTDAgentTest.class,
-    QLearningAgentTest.class})
+@Suite.SuiteClasses({
+    AttributeTest.class,
+    DataSetTest.class,
+    ExampleTest.class,
+    DecisionListTest.class,
+    DecisionTreeTest.class,
+    AdaBoostLearnerTest.class,
+    DecisionListLearnerTest.class,
+    DecisionTreeLearnerTest.class,
+    MajorityLearnerTest.class,
+    NeuralNetworkLearnerTest.class,
+    StumpLearnerTest.class,
+    WeightedMajorityLearnerTest.class,
+    NeuralNetworkTest.class,
+    PerceptronTest.class,
+    PassiveADPAgentTest.class,
+    PassiveTDAgentTest.class,
+    QLearningAgentTest.class,
+    ReinforcementLearningAgentTest.class
+})
 public class LearningTestSuite {
 }
